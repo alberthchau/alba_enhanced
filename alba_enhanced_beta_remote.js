b6c61f5e3d509d885786389583cb2e223cc0ef64
BUCKTHORN\Albert <alberthchau@gmail.com>
2024-12-21 21:01:28 -0800

    fixed missing bracket

--- alba_enhanced_beta_remote.js
+++ alba_enhanced_beta_remote.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         Alba Enhancements Remote BETA
-// @namespace    
+// @namespace
 // @version      1
 // @description  Variety changes to Alba Mobile changes to make it easier to see
-// @match        ://www.mcmxiv.com/alba/mobile/
+// @match        *://www.mcmxiv.com/alba/mobile/*
 // @require      https://raw.githubusercontent.com/alberthchau/alba_enhanced/refs/heads/main/alba_enhanced_beta.js?ts=TIMESTAMP
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    
-    // Dynamically add the cache buster to the required script
-    const scriptUrl = 'https://raw.githubusercontent.com/alberthchau/alba_enhanced/refs/heads/main/alba_enhanced_beta.js';
-    const script = document.createElement('script');
-    script.src = ${scriptUrl}?ts=${Date.now()};
-    document.head.appendChild(script);
+(function () {
+  "use strict";
+
+  // Dynamically add the cache buster to the required script
+  const scriptUrl = "https://raw.githubusercontent.com/alberthchau/alba_enhanced/refs/heads/main/alba_enhanced_beta.js";
+  const script = document.createElement("script");
+  script.src = `${scriptUrl}?ts=${Date.now()}`;
+  document.head.appendChild(script);
 })();
